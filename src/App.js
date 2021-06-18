eb3bf2f3c584e2e1e0c69da8d292f901da9d5f7b
Joshua Hernandez <jedchernandez@gmail.com>
2021-06-17 23:57:57 -0400

    fix: Update the fetch url for image entry

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ class App extends Component {
       .then((response) => response.json())
       .then((response) => {
         if (response) {
-          fetch("https://smart-brain-ap1.herokuapp.com:3000/image", {
+          fetch("https://smart-brain-ap1.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
